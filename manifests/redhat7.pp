5901122f0061da7a96d5a28560c2cc6842d231f9
Jack Coleman <jack@autostructure.io>
2018-09-10 17:27:26 -0400

    2_1_*

--- manifests/redhat7.pp
+++ manifests/redhat7.pp
@@ -18,4 +18,34 @@ class secure_linux_cis::redhat7 {
   include ::secure_linux_cis::redhat7::cis_1_1_1_6
   include ::secure_linux_cis::redhat7::cis_1_1_1_7
   include ::secure_linux_cis::redhat7::cis_1_1_1_8
+
+  include ::secure_linux_cis::redhat7::cis_1_1_2
+  include ::secure_linux_cis::redhat7::cis_1_1_3
+  include ::secure_linux_cis::redhat7::cis_1_1_4
+  include ::secure_linux_cis::redhat7::cis_1_1_5
+  include ::secure_linux_cis::redhat7::cis_1_1_6
+  include ::secure_linux_cis::redhat7::cis_1_1_7
+  include ::secure_linux_cis::redhat7::cis_1_1_8
+  include ::secure_linux_cis::redhat7::cis_1_1_9
+  include ::secure_linux_cis::redhat7::cis_1_1_10
+  include ::secure_linux_cis::redhat7::cis_1_1_11
+  include ::secure_linux_cis::redhat7::cis_1_1_12
+  include ::secure_linux_cis::redhat7::cis_1_1_13
+  include ::secure_linux_cis::redhat7::cis_1_1_14
+  include ::secure_linux_cis::redhat7::cis_1_1_15
+  include ::secure_linux_cis::redhat7::cis_1_1_16
+  include ::secure_linux_cis::redhat7::cis_1_1_17
+  include ::secure_linux_cis::redhat7::cis_1_1_18
+  include ::secure_linux_cis::redhat7::cis_1_1_19
+  include ::secure_linux_cis::redhat7::cis_1_1_20
+  include ::secure_linux_cis::redhat7::cis_1_1_21
+  include ::secure_linux_cis::redhat7::cis_1_1_22
+
+  include ::secure_linux_cis::redhat7::cis_2_1_1
+  include ::secure_linux_cis::redhat7::cis_2_1_2
+  include ::secure_linux_cis::redhat7::cis_2_1_3
+  include ::secure_linux_cis::redhat7::cis_2_1_4
+  include ::secure_linux_cis::redhat7::cis_2_1_5
+  include ::secure_linux_cis::redhat7::cis_2_1_6
+  include ::secure_linux_cis::redhat7::cis_2_1_7
 }
---
+++ manifests/redhat7/cis_1_1_11.pp
@@ -0,0 +1,8 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include secure_linux_cis::redhat7::cis_1_1_11
+class secure_linux_cis::redhat7::cis_1_1_11 {
+}
