62ed37a8bfb0312b5214f38f9b0e6054f5326b9b
Jack Coleman <jack@autostructure.io>
2018-09-18 14:51:53 -0400

    4_1_2-4

--- manifests/redhat7/cis_4_1_17.pp
+++
@@ -1,8 +0,0 @@
-# A description of what this class does
-#
-# @summary A short summary of the purpose of this class
-#
-# @example
-#   include secure_linux_cis::redhat7::cis_4_1_17
-class secure_linux_cis::redhat7::cis_4_1_17 {
-}
--- manifests/redhat7/cis_4_1_2.pp
+++ manifests/redhat7/cis_4_1_2.pp
@@ -1,8 +1,27 @@
-# A description of what this class does
+# 4.1.2 Ensure auditd service is enabled (Scored)
 #
-# @summary A short summary of the purpose of this class
+# Description:
+# Turn on the auditd daemon to record system events.
+#
+# Rationale:
+# The capturing of system events provides system administrators with information to allow
+# them to determine if unauthorized access to their system is occurring.
+#
+# @summary 4.1.2 Ensure auditd service is enabled (Scored)
 #
 # @example
 #   include secure_linux_cis::redhat7::cis_4_1_2
-class secure_linux_cis::redhat7::cis_4_1_2 {
+class secure_linux_cis::redhat7::cis_4_1_2 (
+  Boolean $enforced = true,
+) {
+
+  if $enforced {
+
+    service { 'auditd':
+      ensure => running,
+      enable => true,
+    }
+
+  }
+
 }
