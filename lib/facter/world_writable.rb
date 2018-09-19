e5e28cdcda35aa5bc300f229d424d11b8a70f586
Luther <luther@autostructure.io>
2018-09-19 16:29:22 -0400

    rubocop hates me

--- lib/facter/world_writable.rb
+++ lib/facter/world_writable.rb
@@ -3,7 +3,5 @@
 # which are security risks
 
 Facter.add('world_writable') do
-
-  setcode 'df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -0002',
-
+  setcode "df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -0002"
 end
