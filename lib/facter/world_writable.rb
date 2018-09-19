800176edd04d0072fa56b5f17127d78eaccf4644
Luther <luther@autostructure.io>
2018-09-19 15:28:16 -0400

    6.1.1 - 6.1.10

---
+++ lib/facter/world_writable.rb
@@ -0,0 +1,9 @@
+# world_writable.rb
+# Custom fact that contains all files that are world writable,
+# which are security risks
+
+Facter.add('world_writable') do
+
+  setcode 'df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -0002',
+
+end
