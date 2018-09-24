1f3713d1a3544fd42437894636666bbd6644f97d
Jack Coleman <jack@autostructure.io>
2018-09-24 14:56:31 -0400

    update xorg fact

--- lib/facter/xorg_x11_packages.rb
+++ lib/facter/xorg_x11_packages.rb
@@ -2,6 +2,8 @@
 #
 Facter.add('xorg_x11_packages') do
   confine kernel: :linux
-  package_list = Facter::Core::Execution.exec('rpm -qa xorg-x11*')
-  package_list.split("\n")
+  setcode do
+    package_list = Facter::Core::Execution.exec('rpm -qa xorg-x11*')
+    package_list.split("\n")
+  end
 end
