ff34716899618348a34704c65768c3bb2da90921
Luther <luther@autostructure.io>
2018-09-26 11:40:41 -0400

    sdfg

--- spec/classes/redhat7/cis_6_2_7_spec.rb
+++ spec/classes/redhat7/cis_6_2_7_spec.rb
@@ -14,22 +14,22 @@ describe 'secure_linux_cis::redhat7::cis_6_2_7' do
         if option
           it {
             is_expected.to contain_file('/var/home_directory_cis.sh')
-          #     .with(
-          #       source: 'puppet:///modules/secure_linux_cis/home_directory.sh',
-          #       notify: "Exec['home_dir']"
-          #         .with(
-          #           command: '/usr/./home_directory_cis.sh',
-          #           subscribe: "File['/var/home_directory_cis.sh']",
-          #         ),
-          #     )
-          # }
-          # it {
-          #   is_expected.to contain_exec('home_dir')
-          #     .with(
-          #       command: '/usr/./home_directory_cis.sh',
-          #       subscribe: 'File[\'/var/home_directory_cis.sh\']',
-          #     )
-           }
+            #     .with(
+            #       source: 'puppet:///modules/secure_linux_cis/home_directory.sh',
+            #       notify: "Exec['home_dir']"
+            #         .with(
+            #           command: '/usr/./home_directory_cis.sh',
+            #           subscribe: "File['/var/home_directory_cis.sh']",
+            #         ),
+            #     )
+            # }
+            # it {
+            #   is_expected.to contain_exec('home_dir')
+            #     .with(
+            #       command: '/usr/./home_directory_cis.sh',
+            #       subscribe: 'File[\'/var/home_directory_cis.sh\']',
+            #     )
+          }
         else
           it { is_expected.not_to contain_file('/var/home_directory_cis.sh') }
         end
