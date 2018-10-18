036ec8dc4273ad4c2b370b3e203c15285f84e8a8
Michael Miller <michael@autostructure.io>
2018-10-18 11:52:02 -0400

    5_4_1_4

--- lib/facter/local_users.rb
+++ lib/facter/local_users.rb
@@ -7,22 +7,25 @@ Facter.add(:local_users) do
     user_list = Facter::Core::Execution.exec('egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1').split("\n")
     if user_list
       user_list.each do |user|
+        local_users[user] = {}
+
         maximum_number_of_days_between_password_change = Facter::Core::Execution.exec("chage --list #{user} | grep \"Max\"")
         number_parser_max = %r{\d+}.match(maximum_number_of_days_between_password_change)
-        local_users[user] = {}
 
         minimum_number_of_days_between_password_change = Facter::Core::Execution.exec("chage --list #{user} | grep \"Min\"")
         number_parser_min = %r{\d+}.match(minimum_number_of_days_between_password_change)
-        local_users[user] = {}
 
         warning_number_of_days_between_password_change = Facter::Core::Execution.exec("chage --list #{user} | grep \"warn\"")
         number_parser_warn = %r{\d+}.match(warning_number_of_days_between_password_change)
-        local_users[user] = {}
+
+        password_inactive = Facter::Core::Execution.exec("chage --list #{user} | grep \"inactive\"")
+        number_parser_inactive = %r{\d+}.match(password_inactive)
 
         local_users[user] = {
           'max_days_between_password_change'  => number_parser_max[0].to_i,
           'min_days_between_password_change'  => number_parser_min[0].to_i,
           'warn_days_between_password_change' => number_parser_warn[0].to_i,
+          'password_inactive'                 => number_parser_inactive[0].to_i,
         }
       end
     end
