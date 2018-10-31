ddb542b0069f76dd99cf3bcdd6fc8e8250c50169
Michael Miller <michael@autostructure.io>
2018-10-31 15:04:15 -0400

    attempting to fix expected a non-empty String for first argument error

--- lib/facter/var_tmp_partition.rb
+++ lib/facter/var_tmp_partition.rb
@@ -2,5 +2,5 @@
 # Ensures a seperate partition exists for /var/tmp
 
 Facter.add('var_tmp_partition') do
-  setcode Facter::Core::Execution.exec('mount | grep /var/tmp')
+  setcode Facter::Core::Execution.exec("mount | grep \"/var/tmp\"")
 end
