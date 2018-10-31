0b5cdf016c404cf786fbd53bfd20be28ec4e979d
Michael Miller <michael@autostructure.io>
2018-10-31 15:12:36 -0400

    fixing partition fact errors

--- lib/facter/var_tmp_partition.rb
+++ lib/facter/var_tmp_partition.rb
@@ -2,5 +2,7 @@
 # Ensures a seperate partition exists for /var/tmp
 
 Facter.add('var_tmp_partition') do
-  setcode Facter::Core::Execution.exec("mount | grep \"/var/tmp\"")
+  setcode do
+    Facter::Core::Execution.exec("mount | grep \"/var/tmp\"")
+  end
 end
