4eca68cbbc327a6cdefd4c67863d4f2e02ad826a
Luther <luther@autostructure.io>
2018-10-31 15:13:58 -0400

    local_users if fix

--- lib/facter/local_users.rb
+++ lib/facter/local_users.rb
@@ -30,7 +30,7 @@ Facter.add(:local_users) do
           'max_days_between_password_change'  => number_parser_max[0].to_i,
           'min_days_between_password_change'  => number_parser_min[0].to_i,
           'warn_days_between_password_change' => number_parser_warn[0].to_i,
-          'password_inactive'                 => if number_parser_inactive; number_parser_inactive[0].to_i else '' end,
+          'password_inactive'                 => number_parser_inactive ? number_parser_inactive[0].to_i : '',
           'password_change'                   => password_valid_date,
         }
       end
