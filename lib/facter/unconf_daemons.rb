9a307334b65b79c25247a0863922cd25fe787bb5
Luther <luther@autostructure.io>
2018-10-31 15:21:30 -0400

    unconf_daemons.rb syntax fix

--- lib/facter/unconf_daemons.rb
+++ lib/facter/unconf_daemons.rb
@@ -1,5 +1,5 @@
 # unconf_daemons.rb
 # Ensures no unconfined daemons exist
-Facer.add('unconf_daemons') do
+Facter.add('unconf_daemons') do
   setcode "ps -eZ | egrep \"initrc\" | egrep -vw \"tr|ps|egrep|bash|awk\" | tr ':' ' ' | awk '{ print $NF }'"
 end
