562cd99e495c09d6468939725513d71a81108ac7
Luther <luther@autostructure.io>
2018-10-31 15:47:38 -0400

    fixed parsing error

--- lib/facter/var_tmp_partition_noexec.rb
+++ lib/facter/var_tmp_partition_noexec.rb
@@ -3,9 +3,9 @@
 
 Facter.add('var_tmp_partition_noexec') do
   # rubocop:disable Style/StringLiterals
-  parsed = setcode do
-    Facter::Core::Execution.exec("mount | grep \"/var/tmp\"")
-  end
+  setcode do
+    parsed = Facter::Core::Execution.exec("mount | grep \"/var/tmp\"")
   # rubocop:enable Style/StringLiterals
   %r{noexec}.match(parsed)
+  end
 end
