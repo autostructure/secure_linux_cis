d893bcdf01cb0995362e4cfe09501e1f1b3f347a
Luther <luther@autostructure.io>
2018-11-02 14:13:23 -0400

    travis err 1

--- spec/classes/redhat7/cis_6_2_10_spec.rb
+++ spec/classes/redhat7/cis_6_2_10_spec.rb
@@ -6,7 +6,7 @@ describe 'secure_linux_cis::redhat7::cis_6_2_10' do
   on_supported_os.each do |os, os_facts|
     bool_options.each do |option|
       context "on #{os}" do
-        let(:facts) { os_facts }
+        let(:facts) { { 'dot_file_writable' => 'true' } }
         let(:params) { { 'enforced' => option } }
 
         it { is_expected.to compile }
@@ -18,6 +18,12 @@ describe 'secure_linux_cis::redhat7::cis_6_2_10' do
         else
           it { is_expected.not_to contain_notify('dfw') }
         end
+      end
+      context "on #{os}" do
+        let(:facts) { os_facts }
+        let(:params) { { 'enforced' => option } }
+
+        it { is_expected.to compile }
       end
     end
   end
