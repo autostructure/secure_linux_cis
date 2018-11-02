ec3152f5b42e0a66ae964592efbecc0e8b99cdc7
Michael Miller <michael@autostructure.io>
2018-11-02 15:42:57 -0400

    change Files to files

--- manifests/redhat7/cis_6_2_10.pp
+++ manifests/redhat7/cis_6_2_10.pp
@@ -8,7 +8,7 @@
 #
 # @example
 #   include secure_linux_cis::redhat7::cis_6_2_10
-  class secure_linux_cis::redhat7::cis_6_2_10 (
+class secure_linux_cis::redhat7::cis_6_2_10 (
   Boolean $enforced = true,
 ) {
 
@@ -18,7 +18,7 @@
       owner   => 'root',
       group   => 'root',
       mode    => '0711',
-      content => file('secure_linux_cis/dot_file_wr.sh')
+      content => file('secure_linux_cis/dot_file_wr.sh'),
       # source => 'puppet:///modules/secure_linux_cis/dot_file_wr.sh',
     }
 
