6f260733f84bf60a6ea005da25b0e3a7cbeb0dc7
Michael Miller <michael@autostructure.io>
2018-11-05 11:48:35 -0500

    Revert changew to facts

--- spec/classes/redhat7/cis_6_2_10_spec.rb
+++ spec/classes/redhat7/cis_6_2_10_spec.rb
@@ -6,7 +6,7 @@ describe 'secure_linux_cis::redhat7::cis_6_2_10' do
   on_supported_os.each do |os, os_facts|
     bool_options.each do |option|
       context "on #{os} with option set to #{option}" do
-        let(:facts) { os_facts.merge('dot_file_writable' => 'true') }
+        let(:facts) { os_facts.merge({'dot_file_writable' => 'true'}) }
         let(:params) { { 'enforced' => option } }
 
         it { is_expected.to compile }
