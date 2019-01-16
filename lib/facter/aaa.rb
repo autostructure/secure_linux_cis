7df283009f5feb294ee18f150ba3c364e0b57ee3
Luther <luther@autostructure.io>
2019-01-16 16:14:34 -0500

    loiumjynhtbg

--- lib/facter/aaa.rb
+++ lib/facter/aaa.rb
@@ -1,6 +1,6 @@
 Facter.add('aaa') do
   setcode do
-    password_change = Facter::Core::Execution.exec("chage --list #{user} | grep \"Last\"")
+    password_change = Facter::Core::Execution.exec("chage --list luther | grep \"Last\"")
     number_parser_change = %r{\: ([^:]*)}.match(password_change)
   end
 end
