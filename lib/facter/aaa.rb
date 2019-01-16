db84d87d62f8fb1a37fe08e006292617ce0d6bfe
Luther <luther@autostructure.io>
2019-01-16 16:12:58 -0500

    vfecdsx

--- lib/facter/aaa.rb
+++ lib/facter/aaa.rb
@@ -1,4 +1,4 @@
-Facter.add('home_directory_permission') do
+Facter.add('aaa') do
   setcode do
     password_change = Facter::Core::Execution.exec("chage --list #{user} | grep \"Last\"")
     number_parser_change = %r{\: ([^:]*)}.match(password_change)
