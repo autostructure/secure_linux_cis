951b29e32c202ac60a4a5c4d627d2606e63767e6
Luther <luther@autostructure.io>
2019-01-16 16:38:53 -0500

    98ikfdsa

--- lib/facter/aaa.rb
+++ lib/facter/aaa.rb
@@ -2,9 +2,5 @@ Facter.add('aaa') do
   setcode do
     password_change = Facter::Core::Execution.exec("chage --list luther | grep \"Last\"")
     number_parser_change = %r{\: ([^:]*)}.match(password_change)
-
-    if number_parser_change == ''
-      number_parser_change == 'blank'
-    end
   end
 end
