894864c860a5dc6223c72b8de86f8eed55ffcec8
Luther <luther@autostructure.io>
2019-01-16 16:30:15 -0500

    eqwrety

--- lib/facter/aaa.rb
+++ lib/facter/aaa.rb
@@ -3,10 +3,8 @@ Facter.add('aaa') do
     password_change = Facter::Core::Execution.exec("chage --list luther | grep \"Last\"")
     number_parser_change = %r{\: ([^:]*)}.match(password_change)
 
-    if number_parser_change == '' then
-      echo "yay"
-    else
-      echo "suspicious"
+    if number_parser_change == ''
+      number_parser_change == 'blank'
     end
   end
 end
