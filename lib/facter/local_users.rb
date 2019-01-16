a43924fdc63b0b86d3f8ec1b458543b92aba79d3
Luther <luther@autostructure.io>
2019-01-16 16:00:11 -0500

    tvybuino

--- lib/facter/local_users.rb
+++ lib/facter/local_users.rb
@@ -31,15 +31,15 @@ Facter.add(:local_users) do
         #
         # password_valid_date = Date.parse(number_parser_change[0]) <= Date.today
 
-        local_users[user] = {
-          'max_days_between_password_change'  => number_parser_max[0].to_i,
-          'min_days_between_password_change'  => number_parser_min[0].to_i,
-          'warn_days_between_password_change' => number_parser_warn[0].to_i,
-          'password_inactive'                 => number_parser_inactive ? number_parser_inactive[0].to_i : '',
+        # local_users[user] = {
+        #   'max_days_between_password_change'  => number_parser_max[0].to_i,
+        #   'min_days_between_password_change'  => number_parser_min[0].to_i,
+        #   'warn_days_between_password_change' => number_parser_warn[0].to_i,
+        #   'password_inactive'                 => number_parser_inactive ? number_parser_inactive[0].to_i : '',
           # 'password_change'                   => password_valid_date,
-        }
+        # }
       end
     end
-    local_users
+    # local_users
   end
 end
