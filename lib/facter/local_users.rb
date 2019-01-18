abc49475f5297ebf09712712477b4ee4e6a36a73
Luther <luther@autostructure.io>
2019-01-18 13:39:24 -0500

    ytgrf

--- lib/facter/local_users.rb
+++ lib/facter/local_users.rb
@@ -25,23 +25,17 @@ Facter.add(:local_users) do
         password_change = Facter::Core::Execution.exec("chage --list #{user} | grep \"Last\"")
         number_parser_change = %r{\: ([^:]*)}.match(password_change)
 
-        # fail = 'fail'
-        # if number_parser_change.nil? || number_parser_change == 0 || number_parser_change.empty? || number_parser_change == '' || number_parser_change.blank? || number_parser_change == ': never'
-        #   return fail
-        # end
-
-        # password_valid_date = Date.parse(number_parser_change[0]) <= Date.today
+        password_valid_date = Date.parse(number_parser_change[0]) <= Date.today
 
         local_users[user] = {
           'max_days_between_password_change'  => number_parser_max[0].to_i,
           'min_days_between_password_change'  => number_parser_min[0].to_i,
           'warn_days_between_password_change' => number_parser_warn[0].to_i,
           'password_inactive'                 => number_parser_inactive ? number_parser_inactive[0].to_i : '',
-          # 'password_change'                   => password_valid_date,
+          'password_change'                   => password_valid_date,
         }
       end
     end
-    # local_users
-    password_change
+    local_users
   end
 end
