7aee017514029fb36a34c5ebda147339e66979ec
Luther <luther@autostructure.io>
2019-03-04 17:20:32 -0500

    added variable to avoid loop break

--- manifests/redhat7/cis_5_4_1_5.pp
+++ manifests/redhat7/cis_5_4_1_5.pp
@@ -24,7 +24,7 @@
 
         if !$attributes['password_change'] {
           # fail("User ${user} has a password last changed date in the future. Please investigate.")
-          notify { 'plcd':
+          notify { "plcd ${user}":
             message  => 'Not in compliance with CIS 5.4.1.5 (Scored). We believe the user has a password last changed date in the future. Please investigate.', #lint:ignore:140chars
             loglevel => 'warning',
           }
