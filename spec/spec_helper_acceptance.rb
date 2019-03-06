2589db8f288930c0ff77d0ab46a3f068526cd229
Luther <luther@autostructure.io>
2019-03-06 15:16:02 -0500

    Cleaning up

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,37 +1,8 @@
-<<<<<<< HEAD
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 require 'beaker/puppet_install_helper'
 
 run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-=======
-require 'beaker-pe'
-require 'beaker-puppet'
-require 'beaker-rspec'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
-
-UNSUPPORTED_PLATFORMS = ['windows', 'Darwin'].freeze
-
-run_puppet_install_helper
-configure_type_defaults_on(hosts)
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
-
-unless ENV['RS_PROVISION'] == 'no' || ENV['BEAKER_provision'] == 'no'
-
-  hosts.each do |host|
-    # Need to disable update of ntp servers from DHCP, as subsequent restart of ntp causes test failures
-    if fact_on(host, 'osfamily') == 'Debian'
-      on host, 'dpkg-divert --divert /etc/dhcp-ntp.bak --local --rename --add /etc/dhcp/dhclient-exit-hooks.d/ntp'
-      on host, 'dpkg-divert --divert /etc/dhcp3-ntp.bak --local --rename --add /etc/dhcp3/dhclient-exit-hooks.d/ntp'
-    elsif fact_on(host, 'osfamily') == 'RedHat'
-      on host, 'echo "PEERNTP=no" >> /etc/sysconfig/network'
-    end
-  end
-end
->>>>>>> v1.0.0
 
 RSpec.configure do |c|
   # Project root
@@ -42,7 +13,6 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
-<<<<<<< HEAD
     # Install module and dependencies
     puppet_module_install(source: proj_root, module_name: 'secure_linux_cis')
     hosts.each do |host|
@@ -60,10 +30,6 @@ RSpec.configure do |c|
       on host, puppet('module', 'install', 'puppet-logrotate'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-cron_core'), acceptable_exit_codes: [0, 1]
       on host, puppet('module', 'install', 'puppetlabs-augeas_core'), acceptable_exit_codes: [0, 1]
-=======
-    hosts.each do |host|
-      copy_module_to(host, source: proj_root, module_name: 'php_oci8')
->>>>>>> v1.0.0
     end
   end
 end
